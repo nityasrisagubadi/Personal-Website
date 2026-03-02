fec667a3cb13d60908f9a02fb7625bf345353058
nityasrisagubadi <nsagu@uic.edu>
2026-03-02 13:08:44 -0600

    Added text and image to homescreen

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,24 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-4">
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-      <Link href="/projects">Projects</Link>
+    <div className="flex flex-col gap-6 p-8">
+      
+      <div className="flex gap-6">
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/projects">Projects</Link>
+      </div>
+
+      <h1>Welcome to my Website! </h1>
+      <p>My name is Nitya Sagubadi, I am a freshman in Computer Science at the University of Illinois Chicago. </p>
+
+      <Image
+        src="/Campus-ARC-1090x595.avif"
+        alt="UIC Image"
+        width={500}
+        height={300}
+      />
+      
     </div>
   );
 }
